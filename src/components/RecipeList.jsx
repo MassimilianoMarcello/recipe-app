7ef3646c00cf1b3bee9b69d9a8d0b7f1d4465179
massimiliano <massimarcello@hotmail.it>
2025-01-17 02:08:14 +0100

    almost done

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -17,32 +17,26 @@ const RecipeList = ({ recipes }) => {
     <div className="recipe-list">
       {recipes.map((recipe, index) => (
         <div key={index} className="recipe-card">
-          <h3>{recipe.recipe.label}</h3>
+          <h3 className="recipe-title">{recipe.recipe.label}</h3>
           <img
             src={recipe.recipe.image}
             alt={recipe.recipe.label}
             className="recipe-thumbnail"
           />
           <div className="card-buttons">
-          <button
-            onClick={() => handleViewDetails(recipe)} // Mostra i dettagli
-            className="view-details-button"
-          >
-            View Details
-          </button>
-          <button>
-          <a
-    className="recipe-link"
-    href={recipe.recipe.url}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Go to Recipe
-  </a>
-          </button>
- 
+            <button
+              onClick={() => handleViewDetails(recipe)} // Mostra i dettagli
+              className="view-details-button"
+            >
+              View Details
+            </button>
+            <button
+  className="view-details-button"
+  onClick={() => window.open(recipe.recipe.url, "_blank")}
+>
+  Go to Recipe
+</button>
           </div>
-         
         </div>
       ))}
 
@@ -65,44 +59,43 @@ const RecipeList = ({ recipes }) => {
                   className="recipe-image"
                 />
                 <ul>
-                    <p>Ingredients:</p>
+                  <p>Ingredients:</p>
                   {selectedRecipe.recipe.ingredients.map((ingredient, index) => (
                     <li key={index}>{ingredient.text}</li>
                   ))}
                 </ul>
                 <div className="recipe-details">
-  <div className="recipe-info">
-    <p className="info-item">
-      <strong>Time:</strong> 
-      {selectedRecipe.recipe.totalTime ? `${selectedRecipe.recipe.totalTime} min` : "N/A"}
-    </p>
-    <p className="info-item">
-      <strong>Calories:</strong> 
-      {Math.round(selectedRecipe.recipe.calories)} kcal
-    </p>
-    <p className="info-item">
-      <strong>Cuisine:</strong> 
-      {selectedRecipe.recipe.cuisineType ? selectedRecipe.recipe.cuisineType.join(", ") : "N/A"}
-    </p>
-    <p className="info-item">
-      <strong>Meal Type:</strong> 
-      {selectedRecipe.recipe.mealType ? selectedRecipe.recipe.mealType.join(", ") : "N/A"}
-    </p>
-    <p className="info-item">
-      <strong>Diet:</strong> 
-      {selectedRecipe.recipe.dietLabels ? selectedRecipe.recipe.dietLabels.join(", ") : "N/A"}
-    </p>
-  </div>
-  <a
-    className="recipe-link"
-    href={selectedRecipe.recipe.url}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Go to Recipe
-  </a>
-</div>
-
+                  <div className="recipe-info">
+                    <p className="info-item">
+                      <strong>Time:</strong> 
+                      {selectedRecipe.recipe.totalTime ? `${selectedRecipe.recipe.totalTime} min` : "N/A"}
+                    </p>
+                    <p className="info-item">
+                      <strong>Calories:</strong> 
+                      {Math.round(selectedRecipe.recipe.calories)} kcal
+                    </p>
+                    <p className="info-item">
+                      <strong>Cuisine:</strong> 
+                      {selectedRecipe.recipe.cuisineType ? selectedRecipe.recipe.cuisineType.join(", ") : "N/A"}
+                    </p>
+                    <p className="info-item">
+                      <strong>Meal Type:</strong> 
+                      {selectedRecipe.recipe.mealType ? selectedRecipe.recipe.mealType.join(", ") : "N/A"}
+                    </p>
+                    <p className="info-item">
+                      <strong>Diet:</strong> 
+                      {selectedRecipe.recipe.dietLabels ? selectedRecipe.recipe.dietLabels.join(", ") : "N/A"}
+                    </p>
+                  </div>
+                  <a
+                    className="recipe-link"
+                    href={selectedRecipe.recipe.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Go to Recipe
+                  </a>
+                </div>
               </>
             ) : (
               <p>Loading...</p> // Mostra un messaggio di caricamento se la ricetta non è disponibile
@@ -134,6 +127,7 @@ RecipeList.propTypes = {
 };
 
 export default RecipeList;
+
 
 
 
